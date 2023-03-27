a4bd935d882db1286709fe1226b64eec61497b3a
Nirmi11 <nirminagori06@gmail.com>
2023-03-27 16:53:02 +0530

    Day 1

---
+++ day1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+      The React. js framework is an open-source JavaScript framework and library developed by Facebook. It's used for building interactive user interfaces and web applications quickly and efficiently with significantly less code than you would with vanilla JavaScript.
+      </p>
+        
+    </div>
+  );
+}
+
+export default App;
